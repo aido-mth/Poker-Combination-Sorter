9878fbf420cba9185f0a942c75f4c2e0dd7c3e7d
Vladislav Borbut <vladborbut@mail.ru>
2020-12-08 03:02:59 +0300

    checkers code refactoring

--- checkers.h
+++ checkers.h
@@ -13,20 +13,18 @@ bool CheckFourOfKind(const std::vector<Card> &sorted_cards) {
 
 bool CheckFullHouse(const std::vector<Card> &sorted_cards) {
     std::map<CardRank, size_t> ranks;
-    for (auto &card : sorted_cards) {
-        ranks[card.rank]++;
-    }
-
     size_t count_more_than_two = 0;
     bool has_three = false;
-    for (auto &p : ranks) {
-        if (p.second >= 2) {
-            if (p.second == 3) {
-                has_three = true;
-            }
+
+    for (auto &card : sorted_cards) {
+        ranks[card.rank]++;
+        if (ranks[card.rank] == 2) {
             count_more_than_two++;
+        } else if (ranks[card.rank] == 3) {
+            has_three = true;
         }
     }
+
     return count_more_than_two >= 2 && has_three;
 }
 
@@ -41,10 +39,10 @@ bool CheckFlush(const std::vector<Card> &sorted_cards) {
 
 bool CheckStraight(const std::vector<Card> &sorted_cards) {
     std::set<CardRank> ranks;
-    std::set<CardRank> ranksDuplicate;
+    std::set<CardRank> ranks_duplicate;
     for (auto &card : sorted_cards) {
         ranks.insert(card.rank);
-        ranksDuplicate.insert(card.rank);
+        ranks_duplicate.insert(card.rank);
     }
     size_t counter = 1;
     CardRank prev;
@@ -68,17 +66,17 @@ bool CheckStraight(const std::vector<Card> &sorted_cards) {
         return true;
     }
 
-    if (*ranksDuplicate.rbegin() == CardRank::ACE) {
-        cur = *ranksDuplicate.begin();
+    if (*ranks_duplicate.rbegin() == CardRank::ACE) {
+        cur = *ranks_duplicate.begin();
         if (cur != 2) {
             return false;
         }
-        ranksDuplicate.erase(*ranksDuplicate.begin());
+        ranks_duplicate.erase(*ranks_duplicate.begin());
 
         for (size_t i = 0; i < 3; ++i) {
             prev = cur;
-            cur = *ranksDuplicate.begin();
-            ranksDuplicate.erase(*ranksDuplicate.begin());
+            cur = *ranks_duplicate.begin();
+            ranks_duplicate.erase(*ranks_duplicate.begin());
             if (prev + 1 == cur) {
                 continue;
             } else {
@@ -92,6 +90,7 @@ bool CheckStraight(const std::vector<Card> &sorted_cards) {
 
 bool CheckThreeOfKind(const std::vector<Card> &sorted_cards) {
     std::map<CardRank, size_t> ranks;
+
     for (auto &card : sorted_cards) {
         ranks[card.rank]++;
     }
@@ -101,21 +100,21 @@ bool CheckThreeOfKind(const std::vector<Card> &sorted_cards) {
 
 bool CheckTwoPairs(const std::vector<Card> &sorted_cards) {
     std::map<CardRank, size_t> ranks;
+    size_t count_pairs = 0;
+
     for (auto &card : sorted_cards) {
         ranks[card.rank]++;
-    }
-
-    size_t count_pairs = 0;
-    for (auto &p : ranks) {
-        if (p.second == 2) {
+        if (ranks[card.rank] == 2) {
             count_pairs++;
         }
     }
+
     return count_pairs >= 2;
 }
 
 bool CheckPair(const std::vector<Card> &sorted_cards) {
     std::map<CardRank, size_t> ranks;
+
     for (auto &card : sorted_cards) {
         ranks[card.rank]++;
     }
@@ -128,37 +127,27 @@ bool CheckStraightFlush(const std::vector<Card> &sorted_cards) {
     std::vector<Card> diamonds;
     std::vector<Card> hearts;
     std::vector<Card> spades;
+
     for (auto &el : sorted_cards) {
-        switch (el.suit) {
-            case CardSuit::CLUB:
-                clubs.push_back(el);
-                break;
-            case CardSuit::HEART:
-                hearts.push_back(el);
-                break;
-            case CardSuit::DIAMOND:
-                diamonds.push_back(el);
-                break;
-            case CardSuit::SPADE:
-                spades.push_back(el);
-                break;
+        if (el.suit == CLUB) {
+            clubs.push_back(el);
+        } else if (el.suit == HEART) {
+            hearts.push_back(el);
+        } else if (el.suit == DIAMOND) {
+            diamonds.push_back(el);
+        } else if (el.suit == SPADE) {
+            spades.push_back(el);
         }
     }
 
     bool answer = false;
     if (clubs.size() >= 5) {
         answer |= CheckStraight(clubs);
-    }
-
-    if (hearts.size() >= 5) {
+    } else if (hearts.size() >= 5) {
         answer |= CheckStraight(hearts);
-    }
-
-    if (diamonds.size() >= 5) {
+    } else if (diamonds.size() >= 5) {
         answer |= CheckStraight(diamonds);
-    }
-
-    if (spades.size() >= 5) {
+    } else if (spades.size() >= 5) {
         answer |= CheckStraight(spades);
     }
 
